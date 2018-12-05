ed736201218a77a97e142f559b37367b06081f69
jie.zhang <1317674293@qq.com>
2018-12-05 21:38:19 +0800

    My First project

---
+++ MathFunctions/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jason/MyTest/cmake_tutorial/step7/MathFunctions
+# Build directory: /home/jason/MyTest/cmake_tutorial/step7/MathFunctions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ MathFunctions/mysqrt.cxx
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <math.h>  
+#include <time.h>
+#include "MathFunctions.h"
+#include "TutorialConfig.h"
+//this function copy from InvSqrt
+double mysqrt(double number)  
+{  
+	// if we have both log and exp then use them
+	#if defined (HAVE_LOG) && defined (HAVE_EXP)
+		double result;
+	  result = exp(log(number)*0.5);
+	  printf("use log and exp to compute sqrt\n");
+	  return result;
+	#else // otherwise use an iterative approach
+     float new_guess;
+     float last_guess;
+     
+     if (number < 0) {
+         printf("Cannot compute the square root of a negative number!\n");
+         return -1;
+     }
+     
+     new_guess = 1;
+     do {
+         last_guess = new_guess;
+         new_guess = (last_guess + number / last_guess) / 2;
+         printf("%.15e\n", new_guess);
+     } while (new_guess != last_guess);
+     
+     return new_guess;
+  #endif
+}  
